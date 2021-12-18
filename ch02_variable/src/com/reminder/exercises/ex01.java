1aa2ec5bfb7cef80ab6638e81df61158567c16c4
rhjdev <rhjdev@gmail.com>
2021-12-18 19:30:22 +0900

    variable exercises

---
+++ ch02_variable/src/com/reminder/exercises/ex01.java
@@ -0,0 +1,14 @@
+package com.reminder.exercises;
+
+public class ex01 {
+
+	public static void main(String[] args) {
+		
+		/* 주민등록번호를 출력하고자 할 때 사용하여야 할 자료형 */
+		/* 값이 13자리 정수이므로 long 타입 선택한다. */
+		long regNo = 9912312345678L;
+		System.out.println(regNo);
+
+	}
+
+}
