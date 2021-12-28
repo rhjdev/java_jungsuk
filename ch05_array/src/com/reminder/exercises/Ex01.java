597b3c60d03460f36af9b3cec012eb096a8c564f
rhjdev <rhjdev@gmail.com>
2021-12-28 22:08:12 +0900

    array exercises

---
+++ ch05_array/src/com/reminder/exercises/Ex01.java
@@ -0,0 +1,18 @@
+package com.reminder.exercises;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {10, 20, 30, 40, 50};
+		int sum=0;
+		
+		for(int i=0; i < arr.length; i++) {
+			sum += arr[i];
+		}
+		
+		System.out.println("sum=" + sum);
+
+	}
+
+}
