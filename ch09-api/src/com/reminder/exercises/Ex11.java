c70ac124d1aee0f4fcb8dd9a4fe4ffe9ed23296d
rhjdev <rhjdev@gmail.com>
2022-01-31 22:46:00 +0900

    java.lang package&util classes exercises

---
+++ ch09-api/src/com/reminder/exercises/Ex11.java
@@ -0,0 +1,25 @@
+package com.reminder.exercises;
+
+public class Ex11 {
+	
+	public static String delChar(String src, String delCh) {
+		StringBuffer sb = new StringBuffer(src.length());
+		
+		for(int i=0; i < src.length(); i++) {
+			char ch = src.charAt(i);
+			
+			if(delCh.indexOf(ch) == -1) {
+				sb.append(ch);
+			}
+		}
+		return sb.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println("(1!2@3^4~5)" + " -> " + delChar("(1!2@3^4~5)", "~!@#$%^&*()"));
+		
+		System.out.println("(1 2   3  4\t5)" + " -> " + delChar("(1 2   3  4\t5)", " \t"));
+
+	}
+
+}
